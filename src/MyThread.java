5aeea40f8c5e994f34ca00e568addc4adf0156e5
zxeinc <90633824+zxeinc@users.noreply.github.com>
2023-04-11 08:02:45 +0300

    init

---
+++ src/MyThread.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+/**
+ *
+ * @author user
+ */
+public class MyThread implements Runnable {
+ 
+    public double Tstart;
+    public double Tend;
+    public double Tstep;
+    public double Tresult;  
+    
+    public MyThread(double T_start, double T_end, double T_step){
+        Tstart = T_start;
+        Tend = T_end;
+        Tstep = T_step;
+    };
+
+    public void setTstart(double T_start, double T_end, double T_step) {
+        Tstart = T_start;
+        Tend = T_end;
+        Tstep = T_step;
+    }
+    
+    //Override
+    
+    public void run (){
+        while(Tstart < Tend){
+            if(Tstart + Tstep > Tend){
+                Tstep = Tend - Tstart;
+            }
+            
+            Tresult += ( (Math.sqrt(Tstart) + Math.sqrt(Tstart + Tstep)) /2)*Tstep; //sqrt(x)
+                    Tstart += Tstep;
+        }
+        System.out.println(Tresult);
+    }
+    public double Output() {
+    System.out.println(Tresult);
+    return 0;
+    }
+}
